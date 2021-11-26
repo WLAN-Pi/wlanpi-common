4dcb1ab838f323f871b6d4665e2156eb3cb97c3f
Jiri Brejcha <jirka@jiribrejcha.net>
2021-11-26 22:39:35 +0000

    Must run as root

--- opt/wlanpi-common/wlanpi-reg-domain.sh
+++ opt/wlanpi-common/wlanpi-reg-domain.sh
@@ -24,6 +24,12 @@ DOMAIN=$2
 SCRIPT_NAME=$(echo ${0##*/})
 DEBUG=0
 
+# check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must run as root. Add \"sudo\" please".
+   exit 1
+fi
+
 # just in case we need to debug this script:
 debugger() {
     if [ "$DEBUG" != '0' ];then
