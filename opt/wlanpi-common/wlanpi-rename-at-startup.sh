88b28df3ca2a3d31033ab1324b71a2e93dd01968
Jiri Brejcha <jirka@jiribrejcha.net>
2022-01-21 18:53:02 -0600

    Only change hostname if it hasn't been customised by user

--- opt/wlanpi-common/wlanpi-rename-at-startup.sh
+++ opt/wlanpi-common/wlanpi-rename-at-startup.sh
@@ -4,25 +4,28 @@
 
 # Author: Jiri Brejcha, jirka@jiribrejcha.net, @jiribrejcha
 
+SCRIPT_NAME=$(echo ${0##*/})
+
 # Check if the script is running as root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must run as root. Add \"sudo\" please".
    exit 1
 fi
 
-SCRIPT_NAME=$(echo ${0##*/})
-
 # Get the last 3 chars of eth0 MAC address
 LAST_3_CHARS_MAC=$(sed s/://g /sys/class/net/eth0/address | grep -o '...$')
 
 # Check if we got 3 chars
-if [ ${#LAST_3_CHARS_MAC} -eq 3 ]; then
-    ./wlanpi-hostname.sh set "wlanpi-$LAST_3_CHARS_MAC"
-    if [ "$?" != '0' ]; then
-        logger "($SCRIPT_NAME) Hostname change at startup failed!"
-        exit 1
-    fi
-else
+if [ ${#LAST_3_CHARS_MAC} -ne 3 ]; then
     logger "($SCRIPT_NAME) Failed to parse eth0 MAC address, skipping hostname change!"
     exit 1
+fi
+
+# If hostname matches wlanpi or wlanpi-<3-chars>, change it
+if [ "$HOSTNAME" == "wlanpi" ] || echo "$HOSTNAME" | grep -q "^wlanpi-...$" ; then
+    wlanpi-hostname set "wlanpi-$LAST_3_CHARS_MAC"
+    if [ "$?" != '0' ]; then
+        logger "($SCRIPT_NAME) Hostname change failed!"
+        exit 1
+    fi
 fi
