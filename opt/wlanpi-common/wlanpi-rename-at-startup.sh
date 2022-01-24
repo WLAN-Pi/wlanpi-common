fd88e4d0b6893983c1f9b6915c122ff727202084
Jiri Brejcha <jirka@jiribrejcha.net>
2022-01-24 16:50:14 +0000

    Investigating service start issue

--- opt/wlanpi-common/wlanpi-rename-at-startup.sh
+++ opt/wlanpi-common/wlanpi-rename-at-startup.sh
@@ -4,28 +4,6 @@
 
 # Author: Jiri Brejcha, jirka@jiribrejcha.net, @jiribrejcha
 
-SCRIPT_NAME=$(echo ${0##*/})
-
-# Check if the script is running as root
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must run as root. Add \"sudo\" please".
-    exit 1
-fi
-
 # Get the last 3 chars of eth0 MAC address
-LAST_3_CHARS_MAC=$(sed s/://g /sys/class/net/eth0/address | grep -o '...$')
+LAST_3_CHARS_MAC="123"
 
-# Check if we got 3 chars
-if [ ${#LAST_3_CHARS_MAC} -ne 3 ]; then
-    logger "($SCRIPT_NAME) Failed to parse eth0 MAC address, skipping hostname change!"
-    exit 1
-fi
-
-# If hostname matches wlanpi or wlanpi-<3-chars>, change it
-if [ "$HOSTNAME" == "wlanpi" ] || echo "$HOSTNAME" | grep -q "^wlanpi-...$" ; then
-    wlanpi-hostname set "wlanpi-$LAST_3_CHARS_MAC"
-    if [ "$?" != '0' ]; then
-        logger "($SCRIPT_NAME) Hostname change failed!"
-        exit 1
-   fi
-fi
