a084e79447af99222b5fdd563882ff5790439ee3
Jiri Brejcha <jirka@jiribrejcha.net>
2022-01-27 12:11:13 +0000

    Add wlanpi-update command

---
+++ opt/wlanpi-common/wlanpi-update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Updates the WLAN Pi software and packages
+
+# Author: Jiri Brejcha, jirka@jiribrejcha.net, @jiribrejcha
+
+
+# Check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must run as root. Add \"sudo\" please".
+   exit 1
+fi
+
+# Upgrade packages
+sudo apt update && sudo apt upgrade
