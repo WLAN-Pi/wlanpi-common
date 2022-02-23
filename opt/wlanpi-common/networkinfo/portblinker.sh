ab344c93af7a574d37ddff29f7468e80763d7d96
Jiri Brejcha <jirka@jiribrejcha.net>
2022-02-23 10:39:24 +0000

    Support Port Blinker on WLAN Pi Pro

--- opt/wlanpi-common/networkinfo/portblinker.sh
+++ opt/wlanpi-common/networkinfo/portblinker.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
 
-#Author: Jiri Brejcha, jirka@jiribrejcha.net
-#This is a cable finder tool. Cycles eth0 down and up repeatedly so that you can find the respective port on the switch by watching the switch port LEDs.
+# Author: Jiri Brejcha, jirka@jiribrejcha.net, @jiribrejcha
+# This cable finder tool for WLAN Pi Pro repeatedly bounces eth0 down and up so that you can find the respective port on the switch by watching the switch port LEDs.
 
-#Default number of down & up cycles if not specified explicitly otherwise
+# Default number of down & up cycles if not specified explicitly otherwise
 COUNT=5
-#Default interface name if not specified explicitly otherwise
+# Default interface name if not specified explicitly otherwise
 INTERFACE="eth0"
-#Uses colors in output by default
+# Uses colors in output by default
 COLOR="yes"
+VERSION="2.0.0"
+SCRIPT_NAME="$(basename "$0")"
+
+# Check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must run as root. Add \"sudo\" please".
+   exit 1
+fi
 
 usage(){
-  echo "Usage: $0 [[-c NUMBER_OF_CYCLES ] | [-t TIMEOUT_IN_SECONDS] | [-i INTERFACE_NAME] [-h] | --no-color]"
-  echo ""
-  echo "By default blinks until stopped by CRTL+C."
+    echo "This cable finder tool for WLAN Pi Pro repeatedly bounces eth0 down and up so that you can find the respective port on the switch by watching the switch port LEDs."
+    echo
+    echo "With no arguments passed, it bounces eth0 until stopped by CRTL+C."
+    echo
+    echo "Usage:"
+    echo "  $SCRIPT_NAME -c <number-of-cycles>"
+    echo "  $SCRIPT_NAME -t <timeout-in-seconds>"
+    echo "  $SCRIPT_NAME [options]"
+    echo
+    echo "Options:"
+    echo "  -v, --version   Show version"
+    echo "  -h, --help      Show this screen"
+    echo "  -i <interface>  Interface to bounce"
+    echo "  --no-color      Disable color in the output"
+    echo
+    exit 0
+}
+
+version(){
+    echo "$VERSION"
+    exit 0
 }
 
 NUMBER_OF_PROCESSES=$(pidof -x "portblinker.sh" | wc -w)
@@ -35,19 +61,19 @@ blink_nonstop(){
   echo "Interface: $INTERFACE"
   while true; do
     if  [ "$COLOR" == "yes" ]; then
-      echo -e "\e[91mDown\033[0m"
+      echo -e "\e[91m100 Mbps\033[0m"
     else
       echo "Down"
     fi
-    sudo ifconfig "$INTERFACE" down
-    sleep 3
+    ethtool -s "$INTERFACE" speed 100
+    sleep 6
     if  [ "$COLOR" == "yes" ]; then
-      echo -e "\e[92mUp\033[0m"
+      echo -e "\e[92m1 Gbps\033[0m"
     else
       echo "Up"
     fi
-    sudo ifconfig "$INTERFACE" up
-    sleep 7
+    ethtool -s "$INTERFACE" speed 1000
+    sleep 6
   done
 }
 
@@ -57,14 +83,14 @@ blink_n_times(){
   for (( c=1; c<="$COUNT"; c++ ))
   do
     if  [ "$COLOR" == "yes" ]; then
-      echo -e "\e[91mDown\033[0m"
+      echo -e "\e[91m100 Mbps\033[0m"
     else
       echo "Down"
     fi
     sudo ifconfig "$INTERFACE" down
     sleep 3
     if  [ "$COLOR" == "yes" ]; then
-      echo -e "\e[92mUp\033[0m"
+      echo -e "\e[92m1 Gbps\033[0m"
     else
       echo "Up"
     fi
@@ -79,14 +105,14 @@ blink_n_seconds(){
   timeout --foreground $1 bash <<EOF
   while true; do
     if  [ "$COLOR" == "yes" ]; then
-      echo -e "\e[91mDown\033[0m"
+      echo -e "\e[91m100 Mbps\033[0m"
     else
       echo "Down"
     fi
     sudo ifconfig "$INTERFACE" down
     sleep 3
     if  [ "$COLOR" == "yes" ]; then
-      echo -e "\e[92mUp\033[0m"
+      echo -e "\e[92m1 Gbps\033[0m"
     else
       echo "Up"
     fi
@@ -97,43 +123,36 @@ EOF
 sudo dhclient eth0 &
 }
 
-#If an incomplete argument (complete argument is 2 strings separated by space) was provided display usage, with the exception of --no-color
-if [ "$#" -eq 1 ] && [[ "$*" != *'--no-color'* ]]; then
-  usage
-  exit
-fi
-
-#Was any interface name passed as an argument
+# Was any interface name passed as an argument
 if [[ "$*" == *'-i '* ]]; then
     INTERFACE=$(echo $@ | grep -o '\-i .*' | cut -d " " -f2)
 fi
 
-#Was --no-color argument used
+# Was --no-color argument used
 if [[ "$*" == *'--no-color'* ]]; then
     COLOR="no"
 fi
 
-#Parse arguments
+# Parse arguments
 while [ "$1" != "" ]; do
   case $1 in
-      -c | --count )          shift
-                              blink_n_times $1
-                              exit
-                              ;;
-      -t | --timeout )        shift
-                              blink_n_seconds $1
-                              exit
-                              ;;
-      -h | --help )           usage
-                              exit
-                                ;;
+      -c | --count)          shift
+                             blink_n_times $1
+                             exit
+                             ;;
+      -t | --timeout)        shift
+                             blink_n_seconds $1
+                             exit
+                             ;;
+      -h | --help)           usage ;;
+      -v | --version)        version ;;
       * )
   esac
   shift
 done
 
-#Default behaviour when script is executed without arguments
-#blink_n_times "$COUNT"
+# Default behaviour when script is executed without arguments
+# blink_n_times "$COUNT"
 blink_nonstop
 
 exit 0
