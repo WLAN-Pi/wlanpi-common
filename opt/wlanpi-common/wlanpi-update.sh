f71963e142c67069c16743e0f4a25d9ec8fc5d10
Adrian Granados <adrian@intuitibits.com>
2022-03-16 20:06:00 -0400

    Adds option to wlanpi-update for updating wlanpi-* packages only
    In general, the semantics of the command have changed. Type wlanpi-update -h to see new usage.

--- opt/wlanpi-common/wlanpi-update.sh
+++ opt/wlanpi-common/wlanpi-update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+#
 # Updates the WLAN Pi software and packages
-
-# Author: Jiri Brejcha, jirka@jiribrejcha.net, @jiribrejcha
+#
+# Authors: Jiri Brejcha, jirka@jiribrejcha.net, @jiribrejcha
+#          Adrian Granados, adrian@intuitibits.com, @adriangranados
+#
 
 # Check if the script is running as root
 if [[ $EUID -ne 0 ]]; then
@@ -10,5 +12,42 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Upgrade packages
-sudo apt update && sudo apt upgrade
+upgradable () {
+    timeout 60 apt update > /dev/null 2>&1 && apt list --upgradable 2>/dev/null | grep upgradable || exit 1
+}
+
+upgrade_all () {
+    apt -y upgrade
+}
+
+upgrade () {
+    apt -y --only-upgrade install wlanpi-*
+}
+
+usage () {
+  echo "Usage: wlanpi-update {-a | -h | -u}"
+  echo ""
+  echo "  wlanpi-update -a : upgrades all packages"
+  echo "  wlanpi-update -h : show usage info"
+  echo "  wlanpi-update -u : upgrades wlanpi-* packages"
+  echo "  wlanpi-update : shows upgradable packages"
+  echo ""
+  exit 0
+}
+
+case "$1" in
+  -a)
+      upgrade_all
+      ;;
+  -u)
+      upgrade
+      ;;
+  -h)
+      usage
+      ;;
+  *)
+      upgradable
+      ;;
+esac
+
+exit 0
