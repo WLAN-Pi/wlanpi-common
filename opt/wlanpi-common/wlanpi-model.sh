2dd4670e9c638b2d54eebca6f14b1bcbf5c80f2a
Jiri Brejcha <jirka@jiribrejcha.net>
2022-08-11 18:08:48 +0100

    Lists Wi-Fi adapters

--- opt/wlanpi-common/wlanpi-model.sh
+++ opt/wlanpi-common/wlanpi-model.sh
@@ -43,14 +43,12 @@ debugger() {
 
 # Is it Raspberry Pi 4?
 if grep -q "Raspberry Pi 4 Model B" /proc/cpuinfo; then
-    echo "Raspberry Pi 4"
+    echo "Main board:          Raspberry Pi 4"
     debugger "End script now. Platform is Raspberry Pi 4."
-    exit 0
-fi
 
 # Is it WLAN Pi Pro, MCUzone, or other platform?
 # Powered by CM4?
-if grep -q "Raspberry Pi Compute Module 4" /proc/cpuinfo; then
+elif grep -q "Raspberry Pi Compute Module 4" /proc/cpuinfo; then
     debugger "Powered by CM4"
 
     # Look for Pericom 4-port PCI packet switch in lspci
@@ -60,21 +58,41 @@ if grep -q "Raspberry Pi Compute Module 4" /proc/cpuinfo; then
         # Look for VIA USB hub in lsusb
         if lsusb | grep -q "VIA Labs, Inc. Hub"; then
             debugger "Found VIA Labs USB hub. Potentially WLAN Pi Pro."
-            echo "WLAN Pi Pro"
+            echo "Main board:          WLAN Pi Pro"
             debugger "End script now. Platform is WLAN Pi Pro."
-            exit 0
         fi
     fi
     # Powered by CM4 and no Pro hardware found -> MCUzone
-    LSPCILINES=$(lspci | wc -l)
-    if [ $LSPCILINES -le 2 ]; then
+    LSPCI_LINES=$(lspci | wc -l)
+    if [ $LSPCI_LINES -le 2 ]; then
         debugger "Found less than 2 lines in lspci"
-        echo "MCUzone"
+        echo "Main board:          MCUzone"
         debugger "End script now. Platform is MCUzone."
-        exit 0
     fi
-
 else
     # Not CM4 nor RPi4 -> Unknown platform
     echo "Unknown platform"
+    exit 0
+fi
+
+# List installed Wi-Fi adapters
+USB_WIFI_ADAPTER=$(lsusb | grep -i -E "Wireless|Wi-Fi|Wi_Fi|WiFi" | cut -d " " -f 6-)
+M2_WIFI_ADAPTER=$(lspci | grep -i -E "Wireless|Wi-Fi|Wi_Fi|WiFi" | cut -d " " -f 6-)
+
+IFS="
+"
+if [ -n "$USB_WIFI_ADAPTER" ]; then
+    debugger "Found USB Wi-Fi adapter"
+    for item in $USB_WIFI_ADAPTER
+    do
+        echo "USB Wi-Fi adapter:   $item"
+    done
+fi
+
+if [ -n "$M2_WIFI_ADAPTER" ]; then
+    debugger "Found M.2 Wi-Fi adapter"
+    for item in $USB_WIFI_ADAPTER
+    do
+        echo "M.2 Wi-Fi adapter:   $item"
+    done
 fi
