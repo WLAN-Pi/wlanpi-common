7623b7ac78b1023626d65b9b8f305e5d91284e2d
Josh Schmelzle <josh@joshschmelzle.com>
2026-09-22 16:09:21 -0400

    fix(packaging): configure Raspberry Pi device access

---
+++ tests/package-config-test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+
+grep -q 'groupadd --system' debian/postinst
+grep -q 'usermod --append --groups gpio,spi wlanpi' debian/postinst
+grep -q 'udevadm trigger --subsystem-match=gpio --subsystem-match=spidev' debian/postinst
+grep -q 'User=wlanpi' usr/lib/systemd/system/irtt.service.d/wlanpi-common.conf
+grep -q 'irtt.service.d/wlanpi-common.conf' debian/wlanpi-common.install
